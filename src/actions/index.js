70d8e80e7331336131592ebc35a90d428f41b1b7
Michael Salvati <print@3daddict.com>
2018-10-09 13:03:24 -0700

    sign in completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,26 @@
 import types from './types';
 import axios from 'axios';
 
+// ES5 Version
 export const signUp = (userInfo) => {
     return async (dispatch) => {
         try {
             const resp = await axios.post('http://api.reactprototypes.com/signup', userInfo);
             console.log('Sign up resp: ', resp);
         }
-        catch(error){
+        catch(err){
             console.log('Sign up error: ', err.message);
         }
     }
 };
+
+// ES6 Version
+export const signIn = userInfo => async dispatch => {
+    try {
+        const resp = await axios.post('http://api.reactprototypes.com/signin', userInfo);
+        console.log('Sign in resp: ', resp);
+    }
+    catch(err){
+        console.log('Sign in error: ', err.message);
+    }
+}
